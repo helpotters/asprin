8fe68dc817d256f1eed926dd7531927442922b2c
Paul Lemus <paullemus@protonmail.com>
2023-01-18 14:41:36 -0800

    feat: returns to login page after failing to login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -9,11 +9,19 @@ RSpec.feature 'Login', type: :feature do
     visit root_path
   end
 
-  it 'logins successfully with correct info' do
+  it 'logins with email and password' do
     expect(page.current_path).to eq('/sign_in')
     fill_in('Email', with: user.email)
     fill_in('Password', with: user.password)
     click_on('Log in')
     expect(page.current_path).to eq('/')
+  end
+  it 'returns to login page after failed login' do
+    expect(page.current_path).to eq('/sign_in')
+    fill_in('Email', with: 'bad@email.com')
+    fill_in('Password', with: 'bad')
+
+    click_on('Log in')
+    expect(page.current_path).to eq('/sign_in')
   end
 end
