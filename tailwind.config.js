f98ee34296f99f16324ad9d45900458fc4bbe695
Paul Lemus <paullemus@protonmail.com>
2023-01-19 15:37:08 -0800

    style: added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake"],
+  },
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.haml',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
