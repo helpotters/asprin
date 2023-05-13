fca88e409b09982b6826934506d7c4b39c0b52d4
Paul Lemus <paullemus@protonmail.com>
2023-05-13 16:40:24 -0700

    spec: fixed variable to match factory name

--- spec/system/view_notifications_spec.rb
+++ spec/system/view_notifications_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature 'User views their notifications', type: :feature do
   let(:user) { create(:user) }
   let(:friend) { create(:user, first_name: 'Mr', last_name: 'Friend') }
   context 'friend request' do
-    let!(:friend_request) { create(:friendship_request, user: friend, requested_friend: user) }
+    let!(:friend_request) { create(:friend_request, user: friend, requested_friend: user) }
     let!(:notification) { create(:notification, :new, receipent) }
     it 'shows friend request' do
       sign_in(user)
