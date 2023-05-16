c4e2ae51aa6e4ac1b97440653f8b977446219bcb
Paul Lemus <paullemus@protonmail.com>
2023-05-15 17:22:35 -0700

    refactor: moved user notif spec to /features

--- spec/features/user_notifications_spec.rb
+++ spec/features/user_notifications_spec.rb
@@ -2,16 +2,18 @@
 
 require 'rails_helper'
 
-RSpec.feature 'User views their notifications', type: :feature do
+RSpec.feature 'User Notifications', type: :feature do
   let(:user) { create(:user) }
   let(:friend) { create(:user, first_name: 'Mr', last_name: 'Friend') }
   context 'friend request' do
     let!(:friend_request) { create(:friend_request, user: friend, requested_friend: user) }
-    let!(:notification) { create(:notification, :new, receipent) }
+    let!(:notification) { create(:notification, :new, receipent: user) }
+
     it 'shows friend request' do
       sign_in(user)
 
       within('.notifications') do
+        # TODO: Adjust content expectations
         expect(page).to have_content('New Request')
         expect(page).to have_content('Mr Friend')
       end
