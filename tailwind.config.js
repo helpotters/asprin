f34ab4ad63531e23b150b650365c891dfae6932b
Paul Lemus <paullemus@protonmail.com>
2023-08-13 16:26:12 -0700

    fix: added tailwind path to ViewComponents

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   content: [
     './app/views/**/*.html.haml',
+    './app/components/*.html.haml',
+    './app/components/*.rb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
