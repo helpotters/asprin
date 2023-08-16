aeb448eb86214fcbe8f82659006c3e1016e1861e
Paul Lemus <paullemus@protonmail.com>
2023-08-15 17:39:12 -0700

    feat: added user database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+50.times do
+  User.create(
+    image: Faker::Avatar.image(size: "50x50", bgset: "bg2"),
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6, max_length: 20)
+  )
+end
