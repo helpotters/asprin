389be8da16aea904d0dba1bd97b879e458966b81
Paul Lemus <paullemus@protonmail.com>
2023-08-17 14:04:39 -0700

    feat: added tab functionality and redid post form

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,9 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form"
 export default class extends Controller {
-  static targets = ["button", "input"]
+  static targets = ["button", "input", "tab", "panel"]
   connect() {
+    console.log("Connected")
+    this.initialize();
   }
+
   submit(e){
     e.preventDefault
     this.buttonTarget.click();
@@ -16,5 +19,34 @@ export default class extends Controller {
       console.log("success")
       this.textareaTarget.reset
     }
+  }
+
+  initialize() {
+    this.showTab(0); // Show the first tab initially
+  }
+
+  switchTab(event) {
+    const index = event.currentTarget.dataset.index;
+    this.showTab(index);
+  }
+
+  showTab(index) {
+    console.log(this.tabTargets.length)
+    console.log(this.tabTargets.length)
+    this.tabTargets.forEach((tab, i) => {
+      if (i == index) {
+        tab.classList.add("bg-white", "text-gray-900");
+      } else {
+        tab.classList.remove("bg-white", "text-gray-900");
+      }
+    });
+
+    this.panelTargets.forEach((panel, i) => {
+      if (i == index) {
+        panel.classList.remove("hidden");
+      } else {
+        panel.classList.add("hidden");
+      }
+    });
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"),
+            require("@tailwindcss/forms")
+           ],
   daisyui: {
     themes: ["light"],
   },
