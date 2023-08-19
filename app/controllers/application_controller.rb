c6d02d1ab51545331bf7e8b95c911e8c8cf71047
Paul Lemus <paullemus@protonmail.com>
2023-08-19 10:14:39 -0700

    feat: added first + last name to user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   after_action :update_user_online, if: :user_signed_in?
   before_action :set_results
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   private
 
   def set_results
     @results = []
   end
+
   def update_user_online
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
