79f01c4872487fa747fc8740c660eecd2febebad
Paul Lemus <paullemus@protonmail.com>
2023-08-19 17:07:32 -0700

    refactor: #set_results no longer needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   after_action :update_user_online, if: :user_signed_in?
-  before_action :set_results
   before_action :configure_sign_up_params, if: :devise_controller?
 
   private
-
-  def set_results
-    @results = []
-  end
 
   def update_user_online
   end
