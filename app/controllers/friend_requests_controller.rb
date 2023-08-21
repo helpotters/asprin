c9f320a6372d552e8ceaac0a929e1148ceae2b67
Paul Lemus <paullemus@protonmail.com>
2023-08-21 14:21:16 -0700

    refactor: moved turbo remove to a method

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -6,17 +6,19 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.find(params[:id])
     @friend_request.destroy
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove("notif_#{params[:id]}") }
+      format.turbo_stream { render turbo_stream: remove_request }
       format.html { redirect_to authenticated_root_path }
     end
   end
 
   def update
     @friend_request = FriendRequest.find(params[:id])
+    p @friend_request.user
+    p @friend_request.requested_friend
     Friendship.create(friend: @friend_request.user, user: @friend_request.requested_friend)
     @friend_request.destroy
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove("notif_#{params[:id]}") }
+      format.turbo_stream { render turbo_stream: remove_request }
       format.html { redirect_to authenticated_root_path, notice: "friend_request was successfully destroyed. #{params[:id]}" }
     end
   end
@@ -25,5 +27,9 @@ class FriendRequestsController < ApplicationController
 
   def friend_request_params
     params.permit(:user_id, :requested_friend_id)
+  end
+
+  def remove_request
+    turbo_stream.remove("notif_#{params[:id]}")
   end
 end
