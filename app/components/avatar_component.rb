643507489f827cd95b84149e3f9561026b19fbd1
Paul Lemus <paullemus@protonmail.com>
2023-08-20 20:06:31 -0700

    refactor: improved avatar component interface
    Accessibility: alt text included in img tag
    Encapsulation: less dependencies on outside data
    Validation?: Instance variables reference constants for limitations.
    
    Reference: I used github's Primer Components as a reference and adjusted
    it for my use-case. See:  https://github.com/primer/view_components/blob/main/app/components/primer/beta/avatar.rb
    
    [**system_arguments] is a little risky unless I have something cap it.
    I'll leave it as is for now.

--- app/components/avatar_component.rb
+++ app/components/avatar_component.rb
@@ -1,20 +1,65 @@
 # frozen_string_literal: true
 
 class AvatarComponent < ViewComponent::Base
-  def initialize(user:, size: '10')
-    @user = user
-    @size = size
+  # `Avatar` represents users in asprin
+  #
+  # - Set `size` to proportionately adjsut height and width in pixels
+  #
+  # @accessibility
+  # Images should have text-alt that describe the information or function.
+  # Examples of usage:
+  # - As a link to a user profile, the alt-text should be `@helpotters profile`
+  # rather than `@helpotters`
+
+  DEFAULT_SIZE = 10
+  DEFAULT_SHAPE = :circle
+  SHAPE_OPTIONS = [DEFAULT_SHAPE, :square].freeze
+
+  SIZE_OPTIONS = [16, DEFAULT_SIZE, 24, 32, 40, 48, 80].freeze
+
+  def initialize(src:, alt: nil, size: DEFAULT_SIZE, shape: DEFAULT_SHAPE, href: nil, **system_arguments)
+    @href = href
+    @size = fetch_or_fallback(SIZE_OPTIONS, size, DEFAULT_SIZE)
+    @system_arguments = deny_args(**system_arguments)
+    @shape = set_shape(fetch_or_fallback(SHAPE_OPTIONS, shape, DEFAULT_SHAPE))
+    @src = src
+    @alt = alt
+    @shape = set_shape(shape)
+    @classes = tailwind_classes(
+      system_arguments[:classes],
+      "avatar",
+      "#{@shape}", # adjust this
+      "object-cover",
+      "w-#{@size}",
+      "h-#{@size}"
+    )
   end
 
-  def user_image
-    @user.image unless @user.image.nil?
+  private
+
+  def tailwind_classes(*class_lists)
+    class_lists.compact.join(" ")
   end
 
-  def user_status
-    @user.online? ? "online" : "offline"
+  def fetch_or_fallback(options, selected_value, fallback_value)
+    if options.include?(selected_value)
+      selected_value
+    else
+      fallback_value
+    end
   end
 
-  def user_initials
-    @user&.first_name[0]
+  def deny_args(**args)
+    allowed_keys = [:tag, :src, :alt, :height, :width, :classes]
+    args.select { |key, _| allowed_keys.include?(key) }
+  end
+
+  def set_shape(shape)
+    case shape
+    when :circle
+      "rounded-full"
+    when :square
+      "rounded-md"
+    end
   end
 end
