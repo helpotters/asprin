caa021e1cb967a1366354255817ba6b6fdb79a0f
Paul Lemus <paullemus@protonmail.com>
2023-08-21 13:41:08 -0700

    feat: got turbo streams working on dropdown menu

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -6,8 +6,8 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.find(params[:id])
     @friend_request.destroy
     respond_to do |format|
-      format.html { redirect_to authenticated_root_path, notice: "friend_request was successfully destroyed. #{params[:id]}" }
       format.turbo_stream { render turbo_stream: turbo_stream.remove("notif_#{params[:id]}") }
+      format.html { redirect_to authenticated_root_path }
     end
   end
 
@@ -16,14 +16,14 @@ class FriendRequestsController < ApplicationController
     Friendship.create(friend: @friend_request.user, user: @friend_request.requested_friend)
     @friend_request.destroy
     respond_to do |format|
-      format.html { redirect_to authenticated_root_path, notice: "friend_request was successfully destroyed. #{params[:id]}" }
       format.turbo_stream { render turbo_stream: turbo_stream.remove("notif_#{params[:id]}") }
+      format.html { redirect_to authenticated_root_path, notice: "friend_request was successfully destroyed. #{params[:id]}" }
     end
-
   end
 
   private
-    def friend_request_params
-      params.permit(:user_id, :requested_friend_id)
-    end
+
+  def friend_request_params
+    params.permit(:user_id, :requested_friend_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,38 +2,38 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount Lookbook::Engine, at: "/lookbook"
   end
-  get 'search/suggestions'
-  get 'search/index'
+  get "search/suggestions"
+  get "search/index"
   resources :friend_requests
-  post 'search', to: "search#index", as: "search"
-  post 'search/suggestions', to: "search#suggestions", as: "suggestions"
-  get 'friend_requests/create'
-  get 'friend_requests/destroy'
-  get 'friend_requests/update/:id' => "friend_requests#update", as: "accept_friend_request"
-  get 'friend_requests/destroy/:id' => "friend_requests#destroy", as: "deny_friend_request"
-  get 'notifications/destroy'
-  get 'notifications/index'
-  get 'notifications/feed', to: 'notifications#feed'
-  get 'notifications/update'
+  post "search", to: "search#index", as: "search"
+  post "search/suggestions", to: "search#suggestions", as: "suggestions"
+  get "friend_requests/create"
+  get "friend_requests/destroy"
+  post "friend_requests/update/:id" => "friend_requests#update", as: "accept_friend_request"
+  delete "friend_requests/destroy/:id" => "friend_requests#destroy", as: "deny_friend_request"
+  get "notifications/destroy"
+  get "notifications/index"
+  get "notifications/feed", to: "notifications#feed"
+  get "notifications/update"
   resources :posts
   resources :notifications
   resources :friendships
-  devise_for :users, path: '', controllers: {
-    omniauth_callbacks: 'omniauth_callbacks'
-  }
+  devise_for :users, path: "", controllers: {
+                       omniauth_callbacks: "omniauth_callbacks",
+                     }
 
   devise_scope :user do
     unauthenticated :user do
-      root to: 'devise/sessions#new', as: :unauthenticated_root
-      get '/sign_in' => 'users/sessions#new'
-      get '/sign_up' => 'users/registrations#new'
+      root to: "devise/sessions#new", as: :unauthenticated_root
+      get "/sign_in" => "users/sessions#new"
+      get "/sign_up" => "users/registrations#new"
     end
 
     authenticated :user do
-      root to: 'home#index', as: :authenticated_root
-      get '/sign_out' => 'users/sessions#destroy', method: :delete
-      get '/edit_profile' => 'users/profiles#edit', as: :edit_profile
-      patch '/update_profile', to: 'users/profiles#update', as: :update_profile
+      root to: "home#index", as: :authenticated_root
+      get "/sign_out" => "users/sessions#destroy", method: :delete
+      get "/edit_profile" => "users/profiles#edit", as: :edit_profile
+      patch "/update_profile", to: "users/profiles#update", as: :update_profile
     end
   end
 end
