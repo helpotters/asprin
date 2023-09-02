798f7ea67a7f812aab6b2f46b2de0e570afe2ded
Paul Lemus <paullemus@protonmail.com>
2023-09-01 18:08:28 -0700

    refactor: routes security refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,7 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount Lookbook::Engine, at: "/lookbook"
   end
-  get "search/suggestions"
-  get "search/index"
-  resources :friend_requests
-  post "search", to: "search#index", as: "search"
-  post "search/suggestions", to: "search#suggestions", as: "suggestions"
-  get "friend_requests/create"
-  get "friend_requests/destroy"
-  get "friend_requests/create/:id" => "friend_requests#create", as: "send_friend_request"
-  post "friend_requests/update/:id" => "friend_requests#update", as: "accept_friend_request"
-  delete "friend_requests/destroy/:id" => "friend_requests#destroy", as: "deny_friend_request"
-  get "notifications/destroy"
-  get "notifications/index"
-  get "notifications/feed", to: "notifications#feed"
-  get "notifications/update"
-  resources :posts
-  resources :notifications
-  resources :friendships
+
   devise_for :users, path: "", controllers: {
                        omniauth_callbacks: "omniauth_callbacks",
                      }
@@ -32,10 +16,29 @@ Rails.application.routes.draw do
 
     authenticated :user do
       root to: "home#index", as: :authenticated_root
+      # User
       get "/sign_out" => "users/sessions#destroy", method: :delete
       get "/show_profile/:id" => "users/profiles#show", as: :show_profile
       get "/edit_profile" => "users/profiles#edit", as: :edit_profile
       patch "/update_profile", to: "users/profiles#update", as: :update_profile
+
+      # Resources
+      resources :posts
+      resources :friend_requests
+      resources :notifications
+      resources :friendships
+
+      # User's notification feed
+      get "notifications/feed", to: "notifications#feed"
+
+      # Search
+      post "search", to: "search#index", as: "search"
+      post "search/suggestions", to: "search#suggestions", as: "suggestions"
+
+      # Friend Request Actions
+      get "friend_requests/create/:id" => "friend_requests#create", as: "send_friend_request"
+      post "friend_requests/update/:id" => "friend_requests#update", as: "accept_friend_request"
+      delete "friend_requests/destroy/:id" => "friend_requests#destroy", as: "deny_friend_request"
     end
   end
 end
