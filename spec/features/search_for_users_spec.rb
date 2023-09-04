e789f4739453a4b2cc0d86bf01e6a6fe18e7ef3f
Paul Lemus <paullemus@protonmail.com>
2023-09-03 18:27:28 -0700

    spec: cleaned up useless specs

--- spec/features/search_for_users_spec.rb
+++ spec/features/search_for_users_spec.rb
@@ -13,26 +13,4 @@ RSpec.describe "User Search Integration", type: :feature, js: true do
     expect(suggestions).to have_content(user.full_name)
     expect(suggestions).not_to have_content(other_user.full_name)
   end
-
-  xit "hides suggestions when clicking outside of the search box" do
-    visit root_path
-
-    fill_in "Search...", with: "John"
-    # Click outside the search box to trigger hideSuggestions
-    find("body").click
-
-    expect(page).to have_selector("#suggestions", visible: false)
-  end
-
-  xit "renders search suggestions when Turbo Stream updates" do
-    visit root_path
-
-    fill_in "Search...", with: "J"
-    # Wait for suggestions to appear after fetching
-    expect(page).to have_selector("#suggestions", visible: true)
-
-    suggestions = find("#suggestions")
-    expect(suggestions).to have_content(user1.full_name)
-    expect(suggestions).to have_content(user2.full_name)
-  end
 end
