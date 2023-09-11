a3f285f154f13aed3cdcacf5eca1060fc787e915
Paul Lemus <paullemus@protonmail.com>
2023-09-11 12:25:01 -0700

    feat: improved errors handling in input component
    Not quite globally used yet though.

--- app/components/post_form_component.rb
+++ app/components/post_form_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+# NOTE May need refactoring to simply take in a @post object rather than initializing it by itself.
+# That way, it'll be able to accept errors. As-is, it does not display errors.
 class PostFormComponent < ViewComponent::Base
   def initialize(user:)
     @user = user
   end
-
 end
